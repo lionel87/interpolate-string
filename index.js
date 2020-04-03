7b92522d259c1cf68c16eb4d5a3c577dfcc54050
Bulik László <lt05847@kshad.hu>
2020-04-03 15:04:41 +0200

    fixed pattern, allow linebreaks in placeholder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = (text, context, pattern = /\{\{\s?(.+?)\s?\}\}/g) => text
+module.exports = (text, context, pattern = /\{\{\s?(\S+?)\s?\}\}/g) => text
     .replace(pattern, (_, key) => {
         let value = context;
         for (const p of key.split(".")) {
